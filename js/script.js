2e861538ff59fca3a2471c9e61c9be78d09c3edb
crevanwoo <stasya.job@outlook.com>
2016-12-09 20:30:07 +0200

    changed_allergens

--- js/script.js
+++ js/script.js
@@ -283,9 +283,8 @@ $('.footer_cart').on('click', function () {
     $('.full_cart_products').css('height', $(window).height() - $('header').innerHeight() - $('.full_cart_header').innerHeight() - $('.full_cart_payment').innerHeight() - $('.full_cart_footer').innerHeight())
     productsTransfer(0);
     fixingFooterOnTop();
-    hideMenu();
+    hideMenu();		
 })
-
 
 
 function fixingFooterOnTop() {
@@ -337,8 +336,8 @@ $('.full_cart_close').on('click', function () {
         height: 0,
     })
     $('.full_cart .full_cart_products .product').css('left', '-100%');
-    showMenu();
-
+    showMenu();	
+	
 })
 
 
@@ -759,3 +758,11 @@ function setRightRange(obj) {
 }
 
 /* Range relativity ends !!! */
+
+
+$('.footer_cart').on('click', function() {	
+	if (!header_state) {
+            headerSlideDown();
+            header_state = true;
+        }		
+})
